cadcbe0629d50b79d19a79cf7227e4faa199f2d7
Themixedperson <36366647+Themixedperson@users.noreply.github.com>
2018-05-04 10:09:41 +0300

    Update app.py

--- 2uzd/app.py
+++ 2uzd/app.py
@@ -13,7 +13,7 @@ movies = [ {
                         'Release_date' : '05-2018',
                         'Rating' : '5.3',
                         'Genre' : 'Fantasy',
-			'Album_ID' : '1'
+						'Album_ID' : '1'
                    },
                    {
                         'ID' : '1',
@@ -21,7 +21,7 @@ movies = [ {
                         'Release_date' : '05-2018',
                         'Rating' : 'Not Rated',
                         'Genre' : 'Fantasy',
-			'Album_ID' : '1'
+						'Album_ID' : '1'
                    },
                    {
                         'ID' : '2',
@@ -29,7 +29,7 @@ movies = [ {
                         'Release_date' : '05-2018',
                         'Rating' : '8.0',
                         'Genre' : 'Fantasy',
-			'Album_ID' : '1'
+						'Album_ID' : '1'
                    },
                    {
                         'ID' : '3',
@@ -37,7 +37,7 @@ movies = [ {
                         'Release_date' : '05-2018',
                         'Rating' : '3.75',
                         'Genre' : 'Fantasy',
-			'Album_ID' : '1'
+						'Album_ID' : '1'
                    },
                    {
                         'ID' : '4',
@@ -45,7 +45,7 @@ movies = [ {
                         'Release_date' : '05-2018',
                         'Rating' : 'Not Rated',
                         'Genre' : 'Fantasy',
-			'Album_ID' : '1'
+						'Album_ID' : '1'
                    },
                    {
                         'ID' : '5',
@@ -53,7 +53,7 @@ movies = [ {
                         'Release_date' : '05-2018',
                         'Rating' : 'Not Rated',
                         'Genre' : 'Fantasy',
-			'Album_ID' : '1'
+						'Album_ID' : '1'
                    },
                    {
                         'ID' : '6',
@@ -61,7 +61,7 @@ movies = [ {
                         'Release_date' : '09-2018',
                         'Rating' : 'Not Rated',
                         'Genre' : 'Fantasy',
-			'Album_ID' : '1'
+						'Album_ID' : '1'
                    },
                    {
                         'ID' : '7',
@@ -69,7 +69,7 @@ movies = [ {
                         'Release_date' : '11-2018',
                         'Rating' : 'Not Rated',
                         'Genre' : 'Fantasy',
-			'Album_ID' : '1'
+						'Album_ID' : '1'
                    },
                    {
                         'ID' : '8',
@@ -77,24 +77,27 @@ movies = [ {
                         'Release_date' : '01-2018',
                         'Rating' : 'Not Rated',
                         'Genre' : 'Horror',
-			'Album_ID' : '1'
+						'Album_ID' : '1'
                   }]
+
 #Displays all movies
 #curl -i localhost
 #curl -i localhost/movies?title=<title>
 #curl -i localhost/movies?genre=<genre>
 #Gets movies with raiting equal or higher
 #curl -i localhost/getMovieRating/2.5
-#Displays movie and it album
 #curl -i localhost/movies/<movieID>?embedded=album
 @app.route('/movies', methods=['GET'])
 def hello():
 	if( request.args.get('embedded', '') == "album"):
 		moviesEmb=copy.deepcopy(movies)
 		for i in range(0, len(movies)):
-			r = requests.get('http://web1:81/albums/'+moviesEmb[int(i)]['Album_ID'])
-			r = json.loads(r.text)
-			moviesEmb[int(i)]['Album_ID'] = r
+			try:
+				r = requests.get('http://web1:81/albums/'+moviesEmb[int(i)]['Album_ID'])
+				r = json.loads(r.text)
+				moviesEmb[int(i)]['Album'] = r[0]
+			except:
+				moviesEmb[int(i)]['Album'] = "null"
 		return jsonify(moviesEmb), 200
 	elif( request.args.get('title', '')):
 		foundMovies = []
@@ -117,7 +120,10 @@ def hello():
 			return jsonify({'Error':'Rating has to be between 0-10'}), 404
 	else:
 		return jsonify(movies), 200
-#Get movie by ID curl -i localhost/movies/<movieID> curl -i localhost/movies/<movieID>?embedded=album
+
+#Get movie by ID
+#curl -i localhost/movies/<movieID>
+#curl -i localhost/movies/<movieID>?embedded=album
 @app.route('/movies/<movieID>', methods=['GET'])
 def getMovieByID(movieID):
 	if( request.args.get('embedded', '') == "album"):
@@ -126,9 +132,10 @@ def getMovieByID(movieID):
 		r = json.loads(r.text)
 		moviesEmb[int(movieID)]['Album_ID'] = r
 		return jsonify(moviesEmb[int(movieID)]), 200
-	else:
+	else:	
 		movieByID = [ movie for movie in movies if ( movie['ID'] == movieID)]
 		return jsonify(movieByID), 200
+
 #Creates new movie
 #curl -i -X POST -H "Content-Type: application/json" -d '{"Title": "Venom", "Release_date": "2018", "Rating": "Not Rated", "Genre": "Horror", "Album_ID" : "1"}' localhost/movies
 #curl -i -X POST -H "Content-Type: application/json" -d '{"Title": "Venom", "Release_date": "2018", "Rating": "Not Rated", "Genre": "Horror", "Album" : {"Album" : "1", "Artist" : "Mikutavicius", "Genre of Album" : "yra", "Producer" : "Mikutavicius"}}' localhost/movies?embedded=album
@@ -165,11 +172,18 @@ def newMovie():
 				}
 				movies.append(new_Movie)
 				return jsonify(new_Movie), 201
-#Rate movie curl -i -X PATCH -H "Content-Type: application/json" -d '{"Rating": "5"}' localhost/movies/<movie_id>
+
+#Rate movie
+#curl -i -X PATCH -H "Content-Type: application/json" -d '{"Rating": "5"}' localhost/movies/<movie_id>
 #curl -i -X PATCH -H "Content-Type: application/json" -d '{"Album_ID": "2"}' localhost/movies/<movie_id>
+#curl -i -X PATCH -H "Content-Type: application/json" -d '{"Album": {"Album" : "1", "Artist" : "Mikutavicius", "Genre of Album" : "yra", "Producer" : "Mikutavicius"}}' localhost/movies/<movieID>
 @app.route('/movies/<movieId>', methods=['PATCH'])
 def rateMovie( movieId ):
-		if(request.json['Album_ID']):
+		if( 'Album' in request.json ):
+			r = requests.put('http://web1:81/albums/'+movies[int(movieId)]["Album_ID"], json = {"Album" : request.json['Album']['Album'], "Artist" : request.json['Album']['Artist'], "Genre" : request.json['Album']['Genre of Album'], "Producer" : request.json['Album']['Producer']})
+			r = json.loads(r.text)[0]
+			return jsonify(r), 200
+		elif('Album_ID' in request.json):
 			r = requests.get('http://web1:81/albums/'+request.json['Album_ID'])
 			if r.status_code == 404:
 				return jsonify({'Error' : 'Album not found.'}), 404
@@ -188,22 +202,34 @@ def rateMovie( movieId ):
 				return jsonify(movies[int(movieId)]), 200
 			else:
 				return jsonify({'Error':'Rating has to be between 0-10'}), 404
-#curl -i -X PUT -H "Content-Type: application/json" -d '{"Title": "Venom", "Release_date": "2018", "Rating": "Not Rated", "Genre": "Horror"}' localhost/movies/updated/2
-#curl -i -X PUT -H "Content-Type: application/json" -d '{"Album" : "1", "Artist" : "Mikutavicius", "Genre of Album" : "yra", "Producer" : "Mikutavicius"}' localhost/movies/<movieID>/album
+
+#curl -i -X PUT -H "Content-Type: application/json" -d '{"Title": "Venom", "Release_date": "2018", "Rating": "Not Rated", "Genre": "Horror", "Album_ID": "2"}' localhost/movies/2
+#curl -i -X PUT -H "Content-Type: application/json" -d '{"Title": "Venom", "Release_date": "2018", "Rating": "Not Rated", "Genre": "Horror", "Album": {"Album" : "1", "Artist" : "Mikutavicius", "Genre of Album" : "yra", "Producer" : "Mikutavicius"}}' localhost/movies/2
+
 @app.route('/movies/<movieId>', methods=['PUT'])
 def changeMovie( movieId ):
-	if( request.json['Album']):
-		r = requests.put('http://web1:81/albums/'+movies[int(movieId)]["Album_ID"], json = {"Album" : request.json['Album'], "Artist" : request.json['Artist'], "Genre" : request.json['Genre of Album'], "Producer" : request.json['Producer']})
-		r = json.loads(r.text)
-		return jsonify(r), 200
-	else:
-		changed = [ movie for movie in movies if ( movie['ID'] == movieId)]
+	if( 'Album' in request.json ):
 		movies[int(movieId)]['Title'] = request.json['Title']
 		movies[int(movieId)]['Genre'] = request.json['Genre']
 		movies[int(movieId)]['Rating'] = request.json['Rating']
 		movies[int(movieId)]['Release_date'] = request.json['Release_date']
+		r = requests.put('http://web1:81/albums/'+movies[int(movieId)]["Album_ID"], json = {"Album" : request.json['Album']['Album'], "Artist" : request.json['Album']['Artist'], "Genre" : request.json['Album']['Genre of Album'], "Producer" : request.json['Album']['Producer']})
+		movies[int(movieId)]['Album'] = json.loads(r.text)[0]
 		return jsonify(movies[int(movieId)]), 200
-#Deletes movie by ID curl -i -X DELETE localhost/movies/3
+	else:
+		r = requests.get('http://web1:81/albums/'+request.json['Album_ID'])
+		if r.status_code == 404:
+			return jsonify({'Error' : 'Album not found.'}), 404
+		else:
+			movies[int(movieId)]['Title'] = request.json['Title']
+			movies[int(movieId)]['Genre'] = request.json['Genre']
+			movies[int(movieId)]['Rating'] = request.json['Rating']
+			movies[int(movieId)]['Release_date'] = request.json['Release_date']
+			movies[int(movieId)]['Album_ID'] = request.json['Album_ID']
+			return jsonify(movies[int(movieId)]), 200
+
+#Deletes movie by ID
+#curl -i -X DELETE localhost/movies/3
 @app.route('/movies/<movieId>', methods=['DELETE'])
 def removeMovie( movieId ):
         deleted = [ movie for movie in movies if ( movie['ID'] == movieId)]
@@ -212,5 +238,6 @@ def removeMovie( movieId ):
         else:
                 movies.remove(deleted[0])
                 return jsonify(deleted[0]), 200
+
 if __name__ == "__main__":
         app.run(host="0.0.0.0", debug=True, port=5000)
